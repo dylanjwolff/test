dd4162d9bc38866cb9ecd788adf863c17386eeca
wolffd <wolffdy0@gmail.com>
2024-06-29 11:43:23 +0800

    non-interfering

--- test.c
+++ test.c
@@ -3,15 +3,21 @@
 int main() {
   int x = 0;
   int y = 1;
+  int z = 2;
 
   x = x + y;
   x = y + x;
   x = x + y;
   x = y + x;
 
+  x = x + z;
+  z = y - x;
+
   y = y + x + 5;
   y = y + x + 5;
   y = y + x + 5;
+
+  z = y - x;
 
   fprintf(stderr, "x = %d, y = %d\n", x, y);
   
